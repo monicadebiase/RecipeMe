5071fc2c8614e92ac68c23d83d7840b60fa367fb
emilijabog <81429432+emilijabog@users.noreply.github.com>
2021-03-30 22:19:17 +0200

    uploaded dataset no. 5

---
+++ data.py
@@ -0,0 +1,3 @@
+import pandas as pd
+data = pd.read_csv("C:/Users/emili/OneDrive/Dokumenter/GitHub/RecipeMe/RecipeMe/Food Ingredients and Recipe Dataset with Image Name Mapping.csv/Food Ingredients and Recipe Dataset with Image Name Mapping.csv")
+print(data.head())
